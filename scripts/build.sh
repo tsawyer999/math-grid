abb3f79795a0022c73df57be3f0aab3526efdfce
Tom Sawyer <12345678@hollox.net>
2026-02-01 09:26:49 -0500

    feature: create a script to build the app

---
+++ scripts/build.sh
@@ -0,0 +1,8 @@
+DIST_FOLDER="dist"
+
+rm -rf "$DIST_FOLDER"
+mkdir "$DIST_FOLDER"
+cp source/app.css "$DIST_FOLDER"
+cp source/app.js "$DIST_FOLDER"
+cp source/index.html "$DIST_FOLDER"
+cp source/favicon.ico "$DIST_FOLDER"
