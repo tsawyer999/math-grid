c2a137cdba57ed8d7570985e4e6e9e9de030c58d
Tom Sawyer <12345678@hollox.net>
2026-02-01 09:28:13 -0500

    refactor: build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,15 @@
-DIST_FOLDER="dist"
+#!/bin/bash
+set -e  # Exit on any error
 
+DIST_FOLDER="dist"
+SOURCE_FOLDER="source"
+
+# Create dist folder (removes existing one first)
 rm -rf "$DIST_FOLDER"
 mkdir "$DIST_FOLDER"
-cp source/app.css "$DIST_FOLDER"
-cp source/app.js "$DIST_FOLDER"
-cp source/index.html "$DIST_FOLDER"
-cp source/favicon.ico "$DIST_FOLDER"
+
+# Copy files
+FILES=("app.css" "app.js" "index.html" "favicon.ico")
+for file in "${FILES[@]}"; do
+    cp "$SOURCE_FOLDER/$file" "$DIST_FOLDER"
+done
